2390aa1b682991e41ca191f5defcf7714b7d4109
Wasif Farooq <wasif0332@gmail.com>
2020-07-25 18:05:33 +0500

    setup a general boilerplate with mongoose and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
-      .setVersion('1.0')
-      .addTag('cats')
-      .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const options = new DocumentBuilder()
+        .setTitle('Resume')
+        .setDescription('The Resume API description')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+    await app.listen(app.get('ConfigService').get('port'));
 }
+
 bootstrap();
