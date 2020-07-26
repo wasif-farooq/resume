f78488b8ce8e7b9b923770f32b8734a0e830483c
Wasif Farooq <wasif0332@gmail.com>
2020-07-26 18:57:58 +0500

    compteted the profile api

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { ProfileController } from './controllers/profile.controller';
-import { ProfileService } from './services/profile.service';
+import {Module} from '@nestjs/common';
+import {ProfileController} from './controllers/profile.controller';
+import {ProfileService} from './services/profile.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Profile, ProfileSchema} from "./schemas/profile.schema";
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{
-      name: Profile.name,
-      schema: ProfileSchema
-    }])
-  ],
-  controllers: [ProfileController],
-  providers: [ProfileService],
-  exports: [ProfileService]
+    imports: [
+        MongooseModule.forFeature([{
+            name: Profile.name,
+            schema: ProfileSchema
+        }])
+    ],
+    controllers: [ProfileController],
+    providers: [ProfileService],
+    exports: [ProfileService]
 })
 export class ProfileModule {}
